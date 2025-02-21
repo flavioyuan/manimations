1c45d9d2d10ca1b386723d9f63bd4f40a938e3e6
flavioyuan <fyuangouvea@gmail.com>
2025-02-21 08:40:03 -0300

    ADD: transformations to teste.py

--- my-project/teste.py
+++ my-project/teste.py
@@ -6,15 +6,6 @@ class MobjectPlacement(Scene):
         square = Square()
         triangle = Triangle()
 
-        # place the circle two units left from the origin
-        #circle.move_to(RIGHT)
-        # place the square to the left of the circle
-        #square.next_to(circle, LEFT)
-        # align the left border of the triangle to the left border of the circle
-        #triangle.align_to(circle, LEFT)
-        #square.set_fill(BLUE, opacity=0.5)
-        #square.rotate(PI/4)
-
         self.add(circle, square, triangle)
         self.wait(1)
         self.play(
@@ -23,5 +14,10 @@ class MobjectPlacement(Scene):
         )
         self.wait(1)
         self.play(Create(square))
-        square.rotate(PI/4)
+        self.play(square.animate.set_fill(BLUE, opacity=0.5))
+        self.play(square.animate.rotate(PI/4))
         self.wait(1)
+
+        self.play(square.animate.set_fill(YELLOW, opacity=0.5))
+        self.play(square.animate.rotate(-PI/4))
+        self.play(triangle.animate.set_fill(GREEN_A, opacity=0.5))
